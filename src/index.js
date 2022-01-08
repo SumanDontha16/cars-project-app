e9ccaed8ed49932f33b63eda3d0ba46a2c5c48c7
Dontha Suman <51446173+SumanDontha16@users.noreply.github.com>
2022-01-08 15:13:34 +0000

    Added the StrictMode.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import App from "./App";
 
 ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
 	document.getElementById("root")
 );
